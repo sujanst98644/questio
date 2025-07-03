70aeb038c54db80438389e4c5cddd83e7dedb4d1
sujanst9644 <sujan.st98644@gmail.com>
2025-07-03 21:47:56 +0545

    small changes

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -30,7 +30,7 @@ export default function Homepage() {
   const [showPanel, setShowPanel] = React.useState<Checked>(false);
 
   return (
-    <div className="min-h-screen sm:px-6 py-4 flex flex-row">
+    <div className="min-h-screen sm:px-6 py-4 flex flex-row max-w-[98%] ">
       <div className="w-[70%] px-20">
         <div>
           <DropdownMenu>
@@ -46,27 +46,27 @@ export default function Homepage() {
                 checked={showStatusBar}
                 onCheckedChange={setShowStatusBar}
               >
-                Status Bar
+                solved only
               </DropdownMenuCheckboxItem>
               <DropdownMenuCheckboxItem
                 checked={showActivityBar}
                 onCheckedChange={setShowActivityBar}
                 disabled
               >
-                Activity Bar
+                unsolved only
               </DropdownMenuCheckboxItem>
               <DropdownMenuCheckboxItem
                 checked={showPanel}
                 onCheckedChange={setShowPanel}
               >
-                Panel
+                
               </DropdownMenuCheckboxItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
         {posts.map((post) => (
           <div key={post.id}>
-            <div className=" my-2 p-5 space-y-2 hover:bg-gray-50 transition-colors rounded-lg">
+            <div className=" my-1 p-5 space-y-2 hover:bg-gray-100 transition-colors rounded-lg">
               <div className="flex justify-between text-sm text-muted-foreground">
                 <span>Posted by /{post.author}</span>
                 <span>{post.time}</span>
@@ -74,13 +74,39 @@ export default function Homepage() {
               <h6 className="font-bold">{post.title}</h6>
               <span className="text-muted-foreground">{post.description}</span>
             </div>
-            
+
             <div className="w-full bg-gray-300 h-[1px]"></div>
           </div>
         ))}
       </div>
-      <div>
-        <h5>Recent posts</h5>
+      <div className=" flex flex-col justify-between right-0 rounded-lg w-[30%] ">
+        <div className="flex flex-col items-start p-4 bg-gray-50 rounded-2xl">
+          <h5>Recent posts</h5>
+        </div>
+        <div className="p-4">
+          <ul className="list-none p-0 space-y-1 text-sm font-normal grid grid-cols-3">
+            <li className="hover:underline">
+              <a href="#" className="">
+                Privacy Policy
+              </a>
+            </li>
+            <li className="hover:underline">
+              <a href="#" className="">
+                User Agreement
+              </a>
+            </li>
+            <li className="hover:underline">
+              <a href="#" className="">
+                Accessibility
+              </a>
+            </li>
+            <li className="hover:underline col-span-3">
+              <a href="#" className="">
+                Reddit, Inc. © 2025. All rights reserved.
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
